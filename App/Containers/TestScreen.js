3167428c4f4a2337bd288feb13b1b20c977c2267
Ezra Bathini <ezrabathini@gmail.com>
2017-11-26 18:18:42 +1300

    adds eslint flow

---
+++ App/Containers/TestScreen.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+import { View } from 'react-native'
+
+export default class TestScreen extends Component {
+  render () {
+    return <View style={{ flex: 1, backgroundColor: 'red' }} />
+  }
+}
